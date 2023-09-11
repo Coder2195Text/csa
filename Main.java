fb93c1a662024f47f10faccc6de4ad2d4b132975
Coder2195Text <63069499+Coder2195Text@users.noreply.github.com>
2023-09-11 19:07:34 +0000

    1.2 lesson

--- Main.java
+++ Main.java
@@ -2,7 +2,20 @@ public class Main
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello world.");
-    System.out.print("Goodbye world.");
+    final int a = 1; //cant be changed
+    double b = 3.4; 
+    boolean c = true;
+
+    System.out.print("A: ");
+    System.out.println(a);
+    System.out.print("B: ");
+    System.out.println(b);
+    System.out.print("C: ");
+    System.out.println(c);
+
+    System.out.print("A + B: ");
+    System.out.println(a + b); //implicit casting
+
+    
   }
 }
